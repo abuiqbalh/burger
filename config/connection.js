df6c70dba73bfa8f80868de5773314d2fe1342dd
abuiqbalh <33363982+abuiqbalh@users.noreply.github.com>
2018-05-08 12:10:31 -0400

    this is sql deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_url){
+	var connection=mysql.createConnection(process.env.JAWSDB_url);
+} else{
+	connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
   database: "cat_db"
 });
+}
+
+
 
 // Make connection.
 connection.connect(function(err) {
